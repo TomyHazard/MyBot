f527cff32b57bf4deed4e53786979a54c128c98c
tolga <tolga@DESKTOP-DP95SAA>
2022-08-15 18:50:04 +0300

    Proje dosyası ekle.

---
+++ MyBot/MyBot.py
@@ -0,0 +1,59 @@
+﻿
+#import libraries
+
+import telegram.ext
+import time
+import os
+import telegram
+import datetime
+import requests
+
+timeDate = time.strftime('%d:%m:%Y:%H:%M:%S')
+
+with open('token.txt','r') as f:
+
+    TOKEN = f.read()
+
+
+
+def resim1(update,source):
+    update.message.reply_text('Lütfen bekleyiniz...')
+    resim1 = open('assets/images/resim1.jpg','rb')
+    time.sleep(3)
+    update.message.reply_photo(resim1)
+
+  
+    
+def hello(update,source):
+    update.message.reply_text('Merhaba nasılsınız.')
+    
+
+def havaDurumu(update, source):
+
+    resim = "http://openweathermap.org/img/wn/{}@2x.png"
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=".format('izmir')
+    api = "e7d2c6c38b9ae7616ec4a3579c9313cf&lang=tr"
+    ara = url+api
+    r = requests.get(ara)
+    yaz = r.json()
+    isim = yaz["name"]
+    country = yaz["sys"]["country"]
+    temp = int(yaz["main"]["temp"]-273.15)
+    con = yaz["weather"][0]["description"]
+    tarih = datetime.datetime.now()
+    mesg = "{} ülkesi {} şehiri hava durumu\nsıcaklık:{}ϲ∘\ndurum:{}\nTarih: {}".format(country,isim,temp,con,tarih)
+    time.sleep(3)
+    update.message.reply_text(mesg)
+
+
+
+update = telegram.ext.Updater(TOKEN,use_context=True)
+disp = update.dispatcher
+
+disp.add_handler(telegram.ext.CommandHandler('resim1',resim1))
+disp.add_handler(telegram.ext.CommandHandler('merhaba',hello))
+disp.add_handler(telegram.ext.CommandHandler('havadurumu',havaDurumu))
+
+
+update.start_polling()
+update.idle()
